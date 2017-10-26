4c6682df65a2b9caa3c91b421fcc41a5412f729a
Pawel Swiatek <pmscottapps@gmail.com>
2017-10-26 10:57:06 +0200

    dodano kod

---
+++ src/test/java/pl/ncplus/cgaapptui/CgaApptUiApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.ncplus.cgaapptui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CgaApptUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
